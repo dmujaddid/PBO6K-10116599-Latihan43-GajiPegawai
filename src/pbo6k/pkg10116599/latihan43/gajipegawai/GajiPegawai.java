b5ab448a0ede296bd916ca6560fea172c60704ba
Roby <robyeka@email.unikom.ac.id>
2018-10-24 04:44:34 +0700

    ngoding

---
+++ src/pbo6k/pkg10116599/latihan43/gajipegawai/GajiPegawai.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6k.pkg10116599.latihan43.gajipegawai;
+
+/**
+ *
+ * @author eka
+ */
+public class GajiPegawai {
+    
+    private String nama;
+    private String alamat;
+    private int uangTransport;
+    private int uangTunjangan;
+    private int gajiPokok;
+    private int totalGaji;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public int getUangTransport() {
+        return uangTransport;
+    }
+
+    public void setUangTransport(int uangTransport) {
+        this.uangTransport = uangTransport;
+    }
+
+    public int getUangTunjangan() {
+        return uangTunjangan;
+    }
+
+    public void setUangTunjangan(int uangTunjangan) {
+        this.uangTunjangan = uangTunjangan;
+    }
+
+    public int getGajiPokok() {
+        return gajiPokok;
+    }
+
+    public void setGajiPokok(int gajiPokok) {
+        this.gajiPokok = gajiPokok;
+    }
+
+    public int getTotalGaji() {
+        return totalGaji = uangTunjangan+uangTransport+gajiPokok;
+    }
+
+    public void setTotalGaji(int totalGaji) {
+        this.totalGaji = totalGaji;
+    }
+    
+    public void tampilData (String nama, String alamat, int uangTransport, int uangTunjangan, int gajiPokok, int totalGaji) {
+        
+    }
+    
+}
--- src/pbo6k/pkg10116599/latihan43/gajipegawai/PBO6K10116599Latihan43GajiPegawai.java
+++ src/pbo6k/pkg10116599/latihan43/gajipegawai/PBO6K10116599Latihan43GajiPegawai.java
@@ -7,7 +7,12 @@ package pbo6k.pkg10116599.latihan43.gajipegawai;
 
 /**
  *
- * @author eka
+ * @author 
+ * NAMA     : Robby Eka Purnama
+ * KELAS    : PBO6K
+ * NIM      : 10116599
+ * Deskripsi Program : 
+ * 
  */
 public class PBO6K10116599Latihan43GajiPegawai {
 
@@ -16,6 +21,28 @@ public class PBO6K10116599Latihan43GajiPegawai {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        // Inisialisasi
+        GajiPegawai gajiPegawai = new GajiPegawai();
+        
+        gajiPegawai.setNama("Rizki Adam Kurniawan");
+        gajiPegawai.setAlamat("Jalan Semar dlm 4 No. 72/66");
+        gajiPegawai.setUangTransport(250000);
+        gajiPegawai.setUangTunjangan(300000);
+        gajiPegawai.setGajiPokok(4500000);
+        gajiPegawai.setTotalGaji(5050000);
+        
+        // Output program
+        System.out.println("### Data Gaji Karyawan PT. KAKATU ###");
+        System.out.println("-------------------------------------");
+        System.out.println("Nama Karyawan\t: " +gajiPegawai.getNama());
+        System.out.println("Alamat\t\t: " +gajiPegawai.getAlamat());
+        System.out.println("Uang Transport\t: Rp " +gajiPegawai.getUangTransport());
+        System.out.println("Uang Tunjangan\t: Rp " +gajiPegawai.getUangTunjangan());
+        System.out.println("Gaji Pokok\t: Rp " +gajiPegawai.getGajiPokok());
+        System.out.println("TOTAL GAJI\t: Rp " +gajiPegawai.getTotalGaji());
+        
+        
     }
     
 }
